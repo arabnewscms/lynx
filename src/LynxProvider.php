66e8c9cc614c7a8657ce092d605cc3ba2ab23c82
php anonymous <php.anonymous1@gmail.com>
2022-08-16 22:20:29 +0200

    Add files via upload

---
+++ src/LynxProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace Lynx;
+use Illuminate\Support\ServiceProvider;
+
+//use Phpanonymous\It\Commands\Generate;
+
+class LynxProvider extends ServiceProvider {
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+
+		$this->publishes([__DIR__ .'lang'    => base_path('resources/lang')]);
+		$this->publishes([__DIR__ .'publish' => base_path('/')]);
+
+	}
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+
+		// $this->app->singleton('command.lynx', function ($app) {
+		// 		return new Commands\It;
+		// 	});
+
+		// $this->commands([
+		// 		Commands\Generate::class ,
+		// 	]);
+		//
+	}
+
+	public function provides() {
+		return [
+			//	'command.lynx',
+		];
+	}
+
+}
---
+++ src/lang/en/lynx.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'restored'         => 'data restored successfully',
+	'not_found'        => 'error 404 not found',
+	'deleted'          => 'deleted successfully',
+	'recored_added'    => 'added successfully',
+	'recored_updated'  => 'updated successfully',
+	'need_permission'  => 'You must be a super administrator.',
+	'must_add_rules'   => 'must be add rules method on your parent class or following fillable columns :columns',
+	'successfully'     => 'The data has been queried successfully',
+	'no_data_in_guest' => 'you cannot view data for guest mode',
+];
